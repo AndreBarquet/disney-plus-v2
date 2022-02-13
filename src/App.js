3f4bca8eef413284807cd7ef01ec32a5904c6126
Andre Barquet <andreluiz97.bm@gmail.com>
2022-02-13 11:35:06 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { createTheme } from '@material-ui/core/styles';
+import { Toolbar, Typography, Button, IconButton, Box, AppBar } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+
+import { ThemeProvider } from '@mui/system';
+
+import routes from './config/routes';
+
+const primaryColor = {
+  main: '#6b7281',
+  light: '#757ce8',
+  dark: '#002884',
+  contrastText: '#fff',
+};
+
+const secondaryColor = {
+  light: '#ff7961',
+  main: '#f44336',
+  dark: '#ba000d',
+  contrastText: '#000',
+};
+
+const theme = createTheme({
+  palette: {
+    primary: primaryColor,
+    secondary: secondaryColor,
+  },
+});
 
 function App() {
+  const [currentPage, setCurrentPage] = useState();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme} >
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            {/* colocar a logo aqui */}
+            {/* <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+              <MenuIcon />
+            </IconButton> */}
+            {routes.map(currentRoute => (
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} onClick={() => setCurrentPage(currentRoute?.key)}>
+                {currentRoute?.label}
+              </Typography>
+            ))}
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Box sx={{ flexGrow: 1 }}>
+        { }
+      </Box>
+    </ThemeProvider>
   );
 }
 
---
+++ src/config/routes.js
@@ -0,0 +1,11 @@
+import { Menu } from '@mui/icons-material';
+
+const routes = [
+  {
+    key: 'home',
+    label: 'Dashboard',
+    icon: <Menu />
+  }
+]
+
+export default routes;
